f625357c9c4e664b8fea8a1418cc86aa5cea0368
Konstantin Kuchinin <4642651+coocheenin@users.noreply.github.com>
2020-06-07 20:53:56 +0300

    deleting useless files

--- _drafts/ansible-aws-secgroup-rules-with-report.md
+++
@@ -1,5 +0,0 @@
----
-layout: post
-title:  "Generating AWS Security Group rules from ansible with CSV report"
-tags: [ansible,aws]
----
--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# I have jekyll and dependant ruby gems installed in home dir to keep base system clean
-
-export PATH="$(ruby -r rubygems -e 'puts Gem.user_dir')/bin:$PATH"
-jekyll serve -w
-
